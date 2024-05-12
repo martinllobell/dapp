da103bdab84401fc86ce73c6977008c5bbc39a15
griimreaper <leonelbehnke@gmail.com>
2024-05-12 19:07:16 -0300

    arreglo de vistas a las cards de bienvenida

--- dapp-client/src/components/PlayInvitation/Card.tsx
+++ dapp-client/src/components/PlayInvitation/Card.tsx
@@ -9,7 +9,7 @@ export default function InvitationCard({ card }: InvitationCardProp) {
     const { bgImage, description, leadImage, title } = card
     const [hover, setHover] = useState<boolean>(false)
     return (
-        <div className='w-full flex rounded-lg border border-black/20 my-2 h-40 relative transition duration-300 hover:-translate-y-1 cursor-pointer '
+        <div className='w-full flex rounded-lg my-2 h-40 relative transition duration-300 hover:-translate-y-1 cursor-pointer '
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
         >
@@ -22,14 +22,14 @@ export default function InvitationCard({ card }: InvitationCardProp) {
                 <img src={leadImage} className='absolute max-h-56 min-h-56 object-cover'></img>
             </div>
                 <div className={`absolute transform rounded-lg w-full h-full -bottom-1 duration-300 bg-gradient-to-b ${hover ? 'from-white/0 to-black/100' : 'bg-black/10'}`}></div>
-            <div className='grid grid-cols-6 grid-rows-2 justify-center overflow-hidden items-center h-full'>
-                <div className='flex flex-col items-end justify-end col-span-5 row-span-2'>
-                    <div className={`flex flex-col h-full justify-end items-start w-full p-4 relative -bottom-14 transition duration-300 ${hover ? 'md:-translate-y-12' : ''}`}>
+            <div className='grid grid-cols-6 grid-rows-2 justify-start overflow-hidden items-center w-full h-full'>
+                <div className='flex flex-col min-w-full items-end justify-end col-span-5 row-span-2'>
+                    <div className={`flex flex-col h-full justify-end items-start w-full p-4 relative -bottom-14 transition duration-300 ${hover ? 'md:-translate-y-16 lg:-translate-y-12' : ''}`}>
                         <h2 className='text-2xl font-semibold text-gray-200'>{title}</h2>
-                        <h3 className='absolute top-12 mt-1 font-thin hidden md:block text-sm w-auto text-gray-200'>{description}</h3>
+                        <h3 className='absolute w-3/5 md:w-2/4 lg:w-3/4 top-12 mt-1 font-thin hidden md:block text-sm text-gray-200'>{description}</h3>
                     </div>
                 </div>
-                <div className='flex items-end justify-end h-full row-span-2'>
+                <div className='flex items-end h-full col-span-1 justify-end row-span-2'>
                     <a
                         className={`inline-block rounded-full backdrop-blur-xl bg-white drop-shadow-xl p-2 m-4 transform duration-300 ${hover ? 'text-black ' : 'text-white bg-white/10'}`}
                         href="#MainContent"
