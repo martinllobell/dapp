27fe4cd17a117880151452cb387c83fb75b3abd9
martinllobell <martinllobell.3.0@gmail.com>
2024-05-15 02:30:42 -0300

    new card style and update carousel component

--- dapp-client/src/components/carrousel/Carousel.jsx
+++ dapp-client/src/components/carrousel/Carousel.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import 'flowbite/dist/flowbite.css'; // Importa los estilos de Flowbite
 import { Carousel } from 'flowbite';
-import nba from "../../assets/images/nba.jpg";
-import lol from "../../assets/images/lol.jpg";
-import cs2 from "../../assets/images/cs2.webp";
-import champions from "../../assets/images/champions.jpg";
+import PopularBets from './PopularBets';
+import { basketballMatches, lolMatches, soccerMatches } from './dataMatch';
 
 export default function MyCarousel() {
   const [isDesktop, setIsDesktop] = useState(false);
@@ -16,105 +14,79 @@ export default function MyCarousel() {
       setIsDesktop(e.matches);
     }
 
-    // Llama a la función para inicializar el estado
     handleScreenSizeChange(mediaQuery);
-
-    // Agrega el listener para el cambio de tamaño de la pantalla
     mediaQuery.addListener(handleScreenSizeChange);
 
-    // Limpia el listener cuando el componente se desmonte
     return () => {
       mediaQuery.removeListener(handleScreenSizeChange);
     };
   }, []);
+
   useEffect(() => {
-    // Inicializa el carrusel de Flowbite después de que el componente se haya montado en el DOM
     const myCarousel = new Carousel('.carousel', {
-      // Configuración del carrusel, como cellAlign, wrapAround, etc.
       cellAlign: "center",
       wrapAround: true,
       freeScroll: true,
     });
 
-    // Devuelve una función de limpieza para limpiar cuando el componente se desmonte
     return () => {
-      myCarousel.destroy(); // Limpia el carrusel cuando el componente se desmonte
+      myCarousel.destroy();
     };
-  }, []); // El segundo argumento de useEffect asegura que esto solo se ejecute una vez
+  }, []);
 
   return (
-    <div id="default-carousel" className="relative w-full  mb-[3rem]" data-carousel="slide">
-      {
-        isDesktop ?
-          <div id='carousel_wrapper' className="relative overflow-hidden rounded-lg md:h-64">
-            {/* Primer par de imágenes */}
-            <div className=" duration-300 ease-in-out" data-carousel-item>
-              <img src={nba} className="ml-[7%] bg-top absolute block w-2/5 h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="NBA" />
-              <img src={champions} className="mr-[7%] bg-top absolute block w-2/5 h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg right-0" alt="Champions" />
-            </div>
-            {/* Segundo par de imágenes */}
-            <div className=" duration-300 ease-in-out" data-carousel-item>
-              <div className="img"></div>
-              <img src={lol} className="ml-[7%] bg-top absolute block w-2/5 h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="LOL" />
-              <img src={cs2} className="mr-[7%] bg-top absolute block w-2/5 h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg right-0" alt="CS2" />
-            </div>
-            <div className="duration-300 ease-in-out" data-carousel-item>
-              <img src={lol} className="ml-[7%] bg-top absolute block w-2/5 h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="LOL" />
-              <img src={cs2} className="mr-[7%] bg-top absolute block w-2/5 h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg right-0" alt="CS2" />
-            </div>
+    <div id="default-carousel" className="relative w-full mb-[3rem]" data-carousel="slide">
+      {isDesktop ? (
+        <div id='carousel_wrapper' className="relative overflow-hidden rounded-lg md:h-64">
+          <div className="carousel-item duration-300 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={lolMatches} />
+            <PopularBets selectedMatches={basketballMatches} />
+            <PopularBets selectedMatches={soccerMatches} />
+            <PopularBets selectedMatches={lolMatches} />
           </div>
-          :
-          <div id='carousel_wrapper' className="relative h-64 overflow-hidden rounded-lg md:h-96">
-            {/* Item 1 */}
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src={nba} className="bg-top absolute block w-full h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="NBA" />
-            </div>
-            {/* Item 2 */}
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src={champions} className="bg-top absolute block w-full h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="Champions" />
-            </div>
-            {/* Item 3 */}
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src={lol} className="bg-top absolute block w-full h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="LOL" />
-            </div>
-            {/* Item 4 */}
-            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-              <img src={cs2} className="bg-top absolute block w-full h-full object-cover transition-transform transform scale-95 duration-300 hover:scale-100 rounded-lg" alt="CS2" />
-            </div>
+          <div className="carousel-item duration-300 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={lolMatches} />
+            <PopularBets selectedMatches={basketballMatches} />
+            <PopularBets selectedMatches={soccerMatches} />
+            <PopularBets selectedMatches={lolMatches} />
           </div>
-      }
-      {/* Slider indicators */}
-      {
-        isDesktop ?
-          <div className="absolute z-20 flex -translate-x-1/2 -bottom-4 left-1/2 space-x-3 rtl:space-x-reverse -bottom-8">
-            <button type="button" className="!bg-white/60 w-3 h-2 rounded-sm" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" className="!bg-white/60 w-3 h-2 rounded-sm" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" className="!bg-white/60 w-3 h-2 rounded-sm" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="2"></button>
-            
-          </div> : <div className="absolute z-20 flex -translate-x-1/2  left-1/2 space-x-3 rtl:space-x-reverse -bottom-6">
-            <button type="button" className="!bg-white/60 w-5 h-1.5 rounded-sm" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" className="!bg-white/60 w-5 h-1.5 rounded-sm" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" className="!bg-white/60 w-5 h-1.5 rounded-sm" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-            <button type="button" className="!bg-white/60 w-5 h-1.5 rounded-sm" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+          <div className="carousel-item duration-300 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={lolMatches} />
+            <PopularBets selectedMatches={basketballMatches} />
+            <PopularBets selectedMatches={soccerMatches} />
+            <PopularBets selectedMatches={lolMatches} />
           </div>
-      }
-      {/* Slider controls */}
-      {/* <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button> */}
+        </div>
+      ) : (
+        <div id='carousel_wrapper' className="relative h-64 overflow-hidden rounded-lg md:h-96">
+          <div className="carousel-item hidden duration-700 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={lolMatches} />
+          </div>
+          <div className="carousel-item hidden duration-700 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={soccerMatches} />
+          </div>
+          <div className="carousel-item hidden duration-700 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={basketballMatches} />
+          </div>
+          <div className="carousel-item hidden duration-700 ease-in-out grid grid-cols-4 gap-4" data-carousel-item>
+            <PopularBets selectedMatches={lolMatches} />
+          </div>
+        </div>
+      )}
+
+      {isDesktop ? (
+        <div className="absolute z-20 flex -translate-x-1/2 -bottom-4 left-1/2 space-x-3 rtl:space-x-reverse -bottom-8">
+          <button type="button" className="!bg-white/60 w-3 h-2 rounded-sm" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+          <button type="button" className="!bg-white/60 w-3 h-2 rounded-sm" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+          <button type="button" className="!bg-white/60 w-3 h-2 rounded-sm" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        </div>
+      ) : (
+        <div className="absolute z-20 flex -translate-x-1/2 -bottom-4 left-1/2 space-x-3 rtl:space-x-reverse">
+          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+          <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        </div>
+      )}
     </div>
   );
 }
