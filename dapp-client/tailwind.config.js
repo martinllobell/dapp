7b206b7ab09042ece5657766c414b5033072fc70
griimreaper <leonelbehnke@gmail.com>
2024-05-21 03:27:51 -0300

    setting screen size

--- dapp-client/tailwind.config.js
+++ dapp-client/tailwind.config.js
@@ -14,6 +14,15 @@ export default {
           bg: 'radial-gradient(125% 125% at 50% 10%, #000 40%, #63e 100%)',
         },
       },
+      screens: {
+        xs: "360px",
+        ms: "600px",
+        md: "905px",
+        lg: "1280px",
+        xl: "1440px",
+        xxl: "1899px",
+        xxxl: "1920px",
+      },
     },
   },
 
